83da9cb6ec942ea6cd73d3da2454b39c72706e4d
Damian Staszewski <damian@stachuofficial.pl>
2018-12-29 14:58:29 +0100

    build(gradle): javadoc deployment

--- javadoc-deploy.sh
+++ javadoc-deploy.sh
@@ -5,29 +5,17 @@ set -o errexit
 git config --global user.email "damian@stachuofficial.tv"
 git config --global user.name "Damian Staszewski"
 
-./gradlew javadoc
-
 JAVADOC_PATH=./build/javadocs
 JAVADOC_REPO=https://${GITHUB_TOKEN}@github.com/stachu540/Commandor.git
 JAVADOC_DESTINATION=${JAVADOC_PATH}/${TRAVIS_TAG}
 
 git clone -b gh-pages --single-branch ${JAVADOC_REPO} ${JAVADOC_PATH}
 
-mkdir ${JAVADOC_DESTINATION}
-
-for PROJECT in "api" "glitch" "twitch4j" "jda" "discord4j" "javacord" "docs"; do
-    DIRECTORY=${PROJECT}/build/javadoc
-
-    if [[ PROJECT -eq "docs" ]]; then
-        PROJECT="all"
-    fi
-
-    cp -ar ${DIRECTORY} ${JAVADOC_DESTINATION}/${PROJECT}
-done
+./gradlew javadocDeploy
 
 cd ${JAVADOC_PATH}
 
-#cp -L ./latest/index.html ./${TRAVIS_TAG}/index.html
+cp -L ./latest/index.html ./${TRAVIS_TAG}/index.html
 ln -sfn ./${TRAVIS_TAG} ./latest
 
 git add .
